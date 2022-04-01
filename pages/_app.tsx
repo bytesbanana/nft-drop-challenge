7c0033dbc695e85b13be5f2e24d391553f64350b
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-02 01:16:04 +0700

    update dependency and add bar of progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ProgressBar from '@badrap/bar-of-progress'
+import Router from 'next/router'
+
+const progress = new ProgressBar({
+  size: 10,
+  color: '#3c0a75',
+  className: 'z-50',
+  delay: 500,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
